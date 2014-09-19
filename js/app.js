53ef0e7ee7d5831fbe6a4c0fd614d2d3fc9fe537
faide <michael@faide.net>
2014-09-18 18:42:17 -0700

    Foundation framework and jquery inspection output

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+(function ($) {
+    $(document).ready(function () {
+        $.ajax('jquery.xml', {
+            success: function (data) {
+                console.log(data);
+            }
+        })
+    })
+}(jQuery));
